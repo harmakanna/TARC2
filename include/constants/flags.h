80ed38f34ef8ea2aa7e0dcb3fca1eead977b7d4e
Anna Mayorskiy <72272517+harmakanna@users.noreply.github.com>
2025-07-22 10:22:43 -0400

    Added framing for Looker waking up following passing out in PastTown

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -61,10 +61,10 @@
 #define FLAG_ACCEPTED_SCARED_DADS_REQUEST      0x2F // Unused Flag
 #define FLAG_STEPPED_OUT_OF_CITY_HALL          0x30 // Unused Flag
 #define FLAG_HIDE_SHRINE_OF_PEACE_PRESENT_WILD_MONS 0x31 // Unused Flag
-#define FLAG_HIDE_SHRINE_PEACE_PRESENT_ANABEL    0x32 // Unused Flag
-#define FLAG_HIDE_SHRINE_PEACE_CELEBI    0x33 // Unused Flag
-#define FLAG_UNUSED_0x034    0x34 // Unused Flag
-#define FLAG_UNUSED_0x035    0x35 // Unused Flag
+#define FLAG_HIDE_SHRINE_PEACE_PRESENT_ANABEL  0x32 // Unused Flag
+#define FLAG_HIDE_SHRINE_PEACE_CELEBI          0x33 // Unused Flag
+#define FLAG_HIDE_PAST_TOWN_HALL_ANABEL        0x34 // Unused Flag
+#define FLAG_HIDE_PAST_TOWN_HALL_LOST_GIRL     0x35 // Unused Flag
 #define FLAG_UNUSED_0x036    0x36 // Unused Flag
 #define FLAG_UNUSED_0x037    0x37 // Unused Flag
 #define FLAG_UNUSED_0x038    0x38 // Unused Flag
