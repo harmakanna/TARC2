6196db51e35fef5e79e2171f5d7cad107b3b6aa4
Anna Mayorskiy <72272517+harmakanna@users.noreply.github.com>
2025-07-23 11:02:17 -0400

    Polished post-faint scene
    still need further touch ups graphically, etc. to get it all looking smooth, but thats after i finish up the full intro stuff

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -64,7 +64,7 @@
 #define FLAG_HIDE_SHRINE_PEACE_PRESENT_ANABEL  0x32 // Unused Flag
 #define FLAG_HIDE_SHRINE_PEACE_CELEBI          0x33 // Unused Flag
 #define FLAG_HIDE_PAST_TOWN_HALL_ANABEL        0x34 // Unused Flag
-#define FLAG_HIDE_PAST_TOWN_HALL_LOST_GIRL     0x35 // Unused Flag
+#define FLAG_HIDE_PAST_TOWN_HALL_ESTELLA       0x35 // Unused Flag
 #define FLAG_UNUSED_0x036    0x36 // Unused Flag
 #define FLAG_UNUSED_0x037    0x37 // Unused Flag
 #define FLAG_UNUSED_0x038    0x38 // Unused Flag
