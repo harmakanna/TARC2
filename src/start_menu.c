38480a5b2dc08fb2b0b514d9248ff60eacab5106
Anna Mayorskiy <72272517+harmakanna@users.noreply.github.com>
2025-07-26 11:37:47 -0400

    Adjusted start menu

--- src/start_menu.c
+++ src/start_menu.c
@@ -335,8 +335,8 @@ static void AddStartMenuAction(u8 action)
 
 static void BuildNormalStartMenu(void)
 {
-    if (FlagGet(FLAG_SYS_POKEDEX_GET) == TRUE)
-        AddStartMenuAction(MENU_ACTION_POKEDEX);
+    //if (FlagGet(FLAG_SYS_POKEDEX_GET) == TRUE)
+    //    AddStartMenuAction(MENU_ACTION_POKEDEX);
 
     if (DN_FLAG_DEXNAV_GET != 0 && FlagGet(DN_FLAG_DEXNAV_GET))
         AddStartMenuAction(MENU_ACTION_DEXNAV);
@@ -346,10 +346,10 @@ static void BuildNormalStartMenu(void)
 
     AddStartMenuAction(MENU_ACTION_BAG);
 
-    if (FlagGet(FLAG_SYS_POKENAV_GET) == TRUE)
-        AddStartMenuAction(MENU_ACTION_POKENAV);
+    //if (FlagGet(FLAG_SYS_POKENAV_GET) == TRUE)
+    //    AddStartMenuAction(MENU_ACTION_POKENAV);
 
-    AddStartMenuAction(MENU_ACTION_PLAYER);
+    //AddStartMenuAction(MENU_ACTION_PLAYER);
     //if (FlagGet(FLAG_SYS_QUEST_MENU_GET))
     AddStartMenuAction(MENU_ACTION_QUEST_MENU);
     AddStartMenuAction(MENU_ACTION_SAVE);
