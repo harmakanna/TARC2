b5ef1851834deda1991fe8395a7f3443eb01ee09
harmakanna <harmakanna@gmail.com>
2025-08-04 16:45:05 -0400

    Added loop_last_action movement script, added CutsceneWater map

--- src/script_movement.c
+++ src/script_movement.c
@@ -239,6 +239,13 @@ static void ScriptMovement_TakeStep(u8 taskId, u8 moveScrId, u8 objEventId, cons
         SetMovementScriptFinished(taskId, moveScrId);
         FreezeObjectEvent(&gObjectEvents[objEventId]);
     }
+    else if (nextMoveActionId == MOVEMENT_ACTION_LOOP_LAST_ACTION)
+    {        
+        if (!ObjectEventSetHeldMovement(&gObjectEvents[objEventId], nextMoveActionId))
+        {
+            SetMovementScript(moveScrId, movementScript);
+        }
+    }
     else
     {
         if (!ObjectEventSetHeldMovement(&gObjectEvents[objEventId], nextMoveActionId))
