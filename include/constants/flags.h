0657b767605d19bcaa64273cf4116b727a785559
Anna Mayorskiy <72272517+harmakanna@users.noreply.github.com>
2025-08-07 22:39:27 -0400

    Added Port Cutscenes (get off ship, Magikarp Man)

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -98,8 +98,8 @@
 #define FLAG_RESCUED_BIRCH                       0x52
 #define FLAG_LEGENDARIES_IN_SOOTOPOLIS           0x53
 
-#define FLAG_UNUSED_0x054                    0x54  // Unused Flag
-#define FLAG_UNUSED_0x055                    0x55  // Unused Flag
+#define FLAG_HIDE_PORT_ANABEL                0x54  // Unused Flag
+#define FLAG_PURCHASED_MAGIKARP              0x55  // Unused Flag
 
 #define FLAG_HIDE_CONTEST_POKE_BALL          0x56  // Always set after new game, object it hides is added directly
 #define FLAG_MET_RIVAL_MOM                   0x57
